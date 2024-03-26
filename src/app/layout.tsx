7c4b6b955775f5f98c091b3eb8462c7e9f54df06
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-03-26 00:10:15 -0400

    auth started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+        <body className={inter.className}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import AddExerciseCard from "@/components/AddExerciseCard";
 import ExerciseCard from "@/components/ExerciseCard";
 import { db } from "@/lib/db";
+import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
   // testing loading some data
   const users = await db.query.users.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-8">
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
       <ul>
         {users.map((u) => {
           return (

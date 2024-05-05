71b63b5cb6497187d35290a5582012c21a72a79b
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-05-05 00:33:20 -0400

    url fix

--- src/server/helper/url.ts
+++ src/server/helper/url.ts
@@ -4,14 +4,15 @@ export function getBaseURL(): string {
     return `http://localhost:${process.env.PORT ?? 3000}`;
   } else {
     // For production, use the Vercel URL
-    const vercelUrl = process.env.VERCEL_URL;
-    if (vercelUrl) {
-      return `https://${vercelUrl}`;
-    }
+    // const vercelUrl = process.env.VERCEL_URL;
+    // if (vercelUrl) {
+    //   return `https://${vercelUrl}`;
+    // }
 
-    // If VERCEL_URL is not available, use the hostname from the request
-    const hostname = window.location.hostname;
-    const isHttps = window.location.protocol.startsWith("https");
-    return `${isHttps ? "https" : "http"}://${hostname}`;
+    // // If VERCEL_URL is not available, use the hostname from the request
+    // const hostname = window.location.hostname;
+    // const isHttps = window.location.protocol.startsWith("https");
+    // return `${isHttps ? "https" : "http"}://${hostname}`;
+    return "https://weights-ai.vercel.app";
   }
 }

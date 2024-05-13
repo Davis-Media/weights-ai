b250499564805ce38b2974619882f3ee645d3d1e
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-05-13 13:47:04 -0400

    langfuse test

--- src/server/ai/index.ts
+++ src/server/ai/index.ts
@@ -1,8 +1,11 @@
 import { createOpenAI } from "@ai-sdk/openai";
+import { observeOpenAI } from "langfuse";
 
 // export const groq = createOpenAI({
 //   baseURL: "https://api.groq.com/openai/v1",
 //   apiKey: env.GROQ_API_KEY,
 // });
 
-export const openai = createOpenAI();
+export const openai = observeOpenAI(createOpenAI(), {
+  traceId: "weights-ai-main-openai",
+});

d88474bab7b9d34118c9a0f93ebe4de1a2fbb5a5
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-05-17 14:33:27 -0400

    added in embeddings

---
+++ src/app/playground-client/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { api } from "@/trpc/react"
+
+export default function Page() {
+    const addEmbeddingsMutation = api.addEmbeddings.useMutation({
+        onSuccess: () => {
+            alert("Embeddings added")
+        }
+    })
+
+    return <Button onClick={() => addEmbeddingsMutation.mutate()} disabled={addEmbeddingsMutation.isPending}>Add Embeddings</Button>
+}
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { authProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 import { exerciseRouter } from "./exercise";
 import { scheduleRouter } from "./schedule";
 import { setsRouter } from "./sets";
@@ -7,6 +7,11 @@ import { workoutRouter } from "./workout";
 import { userRouter } from "./user";
 import { feedbackRouter } from "./feedback";
 import { subscriptionRouter } from "./subscription";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../sb/database.types";
+import { env } from "@/env";
+import { openaiEmbeddingModel } from "../ai";
+import { embed } from "ai";
 
 export const appRouter = createTRPCRouter({
   workout: workoutRouter,
@@ -16,6 +21,38 @@ export const appRouter = createTRPCRouter({
   schedule: scheduleRouter,
   user: userRouter,
   feedback: feedbackRouter,
+  addEmbeddings: authProcedure.mutation(async () => {
+    // FILL THESE IN WHEN U USE THIS
+    const url = "";
+    const service_role_key = "";
+
+    // get the full user exercise table from DB
+    const sbServerClient = createClient<Database>(
+      url,
+      service_role_key,
+    );
+
+    const { data } = await sbServerClient.from("user_exercise").select();
+
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        const exercise = data[i];
+
+        if (exercise.name_openai_embedding === null) {
+          const { embedding } = await embed({
+            model: openaiEmbeddingModel,
+            value: exercise.name,
+          });
+
+          await sbServerClient.from("user_exercise").update({
+            name_openai_embedding: JSON.stringify(embedding),
+          }).eq("id", exercise.id);
+        }
+      }
+    }
+
+    return true;
+  }),
   hello: publicProcedure
     .input(
       z.object({
